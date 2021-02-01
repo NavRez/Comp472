1d8742bf7d40f1ac1be8f9a59ea15a9646fafdc6
navid <navid@universus.ca>
2021-02-01 13:03:26 -0500

    Added basic file framework

--- hello.py
+++ hello.py
@@ -1,5 +1,26 @@
-import sklearn
+from sklearn.naive_bayes import GaussianNB
+from codecs import open
+import numpy as np
+#from __future__ import division
 
-print("hello world")
+def read_documents(doc_file):
+    docs = []
+    labels = []
+    with open(doc_file, encoding='utf-8') as f:
+        for line in f:
+            words = line.strip().split()
+            docs.append(words[3:])
+            labels.append(words[1])
+    return docs, labels
 
-i = 5 
+if __name__ == "__main__":
+
+    all_docs, all_labels = read_documents("dataset.txt")
+
+    split_point = int(0.80*len(all_docs))
+    train_docs = all_docs[:split_point]
+    train_labels = all_labels[:split_point]  
+    eval_docs = all_docs[split_point:]
+    eval_labels = all_labels[split_point:]
+
+
