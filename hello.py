cfe47ceedc157446445cf6f5b30081b68b194f86
navid <navid@universus.ca>
2021-02-01 21:24:33 -0500

    computed word count

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import LabelEncoder
 from codecs import open
 import numpy as np
 #from __future__ import division
@@ -9,7 +10,10 @@ def read_documents(doc_file):
     with open(doc_file, encoding='utf-8') as f:
         for line in f:
             words = line.strip().split()
-            docs.append(words[3:])
+            new_words = list()
+            for word in words[3:]:
+                new_words.append(str(word))
+            docs.append(new_words)
             labels.append(words[1])
     return docs, labels
 
@@ -22,5 +26,24 @@ if __name__ == "__main__":
     train_labels = all_labels[:split_point]  
     eval_docs = all_docs[split_point:]
     eval_labels = all_labels[split_point:]
+
+    from collections import Counter
+    freqsPos = Counter()
+    freqsNeg = Counter()
+
+    import itertools
+    poSize = 0
+    negSize = 0
+    for (doc,label) in zip(train_docs,train_labels):
+        for w in doc:
+            if label == "pos":
+                freqsPos[w] += 1
+                poSize +=1
+            else:
+                freqsNeg[w] += 1
+                negSize += 1
+        
+    print(poSize)
+    print(negSize)
 
 
