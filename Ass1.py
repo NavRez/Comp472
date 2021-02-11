4fe60836efc87c03ddbf5aade0f82166c63c3d5c
navid <navid@universus.ca>
2021-02-11 15:17:17 -0500

    overwriting save to rebase from secondNavid branch

--- Ass1.py
+++ Ass1.py
@@ -1,5 +1,6 @@
 from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import LabelEncoder
+from sklearn import tree
 from codecs import open
 from gensim import corpora 
 import pandas as pd
@@ -61,137 +62,117 @@ def set_scores(eval_labels,comp_List,f):
     f.write("\nconfusion matrix : \n %s\n" %(confuse))
     f.close()
 
+def listify(df,tdict):
+        newlist = []
+        counter = 0
+        print("Beginning conversion...")
+        for l in df:
+            tmplist = []
+            for word in l:
+                # append to intermediate list the id for the given word under observation
+                tmplist.append(tdict.token2id[word])
+            # convert to numpy array and append to main list
+            newlist.append(np.array(tmplist).astype(float)) # type float
+            print("converted line : " + str(counter))
+            counter+=1
+        return newlist
+
 if __name__ == "__main__":
 
     all_docs, all_labels = read_documents("all_sentiment_shuffled.txt")
 
-    choice = input("Enter your tree of choice \n1. Bayes\n2. DT\n3. DT Best\n")
+    
+    #setting up matrix and numerical list
+    print("setting up matrix ...")
+    df = pd.DataFrame(all_docs).to_numpy(na_value="")
+    tdict = corpora.Dictionary(df)
+    newlist = listify(df,tdict)
 
-    if choice == "1":
+    split_point = int(0.80*len(all_docs))
+    train_docs = all_docs[:split_point]
+    train_labels = all_labels[:split_point]  
+    eval_docs = all_docs[split_point:]
+    eval_labels = all_labels[split_point:]
 
+    ####################### Naive starts
+    print("Printing Naive Bayes : acc,precision, recall , f1 and confuse in that order")
+    from collections import Counter
+    freqsPosWords = Counter()
+    freqsNegWords = Counter()
+    freqs = Counter()
+
+    import itertools
+    poSize = 0
+    negSize = 0
+    posWords = 0
+    negWords = 0
+    
+    for (doc,label) in zip(train_docs,train_labels):
+        if label == "pos":
+            for w in doc:
+                freqsPosWords[w] += 1
+                freqs[w] +=1
+                posWords+=1
+            poSize +=1
+        else:
+            for w in doc:
+                freqsNegWords[w] += 1
+                freqs[w] +=1
+                negWords+=1
+            negSize +=1
         
-        split_point = int(0.80*len(all_docs))
-        train_docs = all_docs[:split_point]
-        train_labels = all_labels[:split_point]  
-        eval_docs = all_docs[split_point:]
-        eval_labels = all_labels[split_point:]
 
-        from collections import Counter
-        freqsPosWords = Counter()
-        freqsNegWords = Counter()
-        freqs = Counter()
+    comp_List = list()
+    f= open("Naive-Bayes-dataset.txt","w")
+    intCounter = len(train_labels) 
+    for truedoc in eval_docs:
+        intCounter+=1
+        output_string = logarithm(freqsPosWords,freqsNegWords,poSize,negSize,posWords,negWords,len(freqs),truedoc) 
+        comp_List.append(output_string)
+        f.write("%d,%s\n" % (intCounter,output_string))
 
-        import itertools
-        poSize = 0
-        negSize = 0
-        posWords = 0
-        negWords = 0
-        
-        for (doc,label) in zip(train_docs,train_labels):
-            if label == "pos":
-                for w in doc:
-                    freqsPosWords[w] += 1
-                    freqs[w] +=1
-                    posWords+=1
-                poSize +=1
-            else:
-                for w in doc:
-                    freqsNegWords[w] += 1
-                    freqs[w] +=1
-                    negWords+=1
-                negSize +=1
-            
+    set_scores(eval_labels,comp_List,f)
+    print("Printing of Naive Bayes complete\n\n")
+    ####################### Naive ends
+    
+    ### DT delimitation ###
 
-        comp_List = list()
-        f= open("Naive-Bayes-dataset.txt","w")
-        intCounter = len(train_labels) 
-        for truedoc in eval_docs:
-            intCounter+=1
-            output_string = logarithm(freqsPosWords,freqsNegWords,poSize,negSize,posWords,negWords,len(freqs),truedoc) 
-            comp_List.append(output_string)
-            f.write("%d,%s\n" % (intCounter,output_string))
+    ####################### DT-Base starts
+    print("Printing DT-Base : acc,precision, recall , f1 and confuse in that order")
+    clf  = tree.DecisionTreeClassifier(criterion="entropy")
 
-        set_scores(eval_labels,comp_List,f)
-        
-    elif choice =="2":
-        from sklearn import tree
-        clf  = tree.DecisionTreeClassifier(criterion="entropy")
-        df = pd.DataFrame(all_docs).to_numpy(na_value="")
+    split_point = int(0.80*len(newlist))
+    train_docs = newlist[:split_point]
+    eval_docs = newlist[split_point:]
 
-        # convert your list of lists into token id's. For example, 'aa bb' could be represented as a 2, a as a 1, etc.
-        tdict = corpora.Dictionary(df)
+    clf = clf.fit(train_docs,train_labels)
+    comp_List = clf.predict(eval_docs)
 
-        newlist = []
-        counter = 0
-        for l in df:
-            tmplist = []
-            for word in l:
-                # append to intermediate list the id for the given word under observation
-                tmplist.append(tdict.token2id[word])
-            # convert to numpy array and append to main list
-            newlist.append(np.array(tmplist).astype(float)) # type float
-            print(counter)
-            counter+=1
+    f= open("DT-Entropy-dataset.txt","w")
+    intCounter = len(train_labels) 
+    for truelabel in comp_List:
+        intCounter+=1 
+        f.write("%d,%s\n" % (intCounter,truelabel))
 
-        split_point = int(0.80*len(newlist))
-        train_docs = newlist[:split_point]
-        train_labels = all_labels[:split_point]  
-        eval_docs = newlist[split_point:]
-        eval_labels = all_labels[split_point:]
+    set_scores(eval_labels,comp_List,f)
+    print("Printing of DT-Base complete\n\n")
+    ####################### DT-Base ends
 
-        
-        clf = clf.fit(train_docs,train_labels)
-        comp_List = clf.predict(eval_docs)
+    ####################### DT-Best starts
+    print("Printing DT-Best : acc,precision, recall , f1 and confuse in that order")
+    clf  = tree.DecisionTreeClassifier(splitter="random")
+    clf = clf.fit(train_docs,train_labels)
+    comp_List = clf.predict(eval_docs)
 
-        f= open("DT-Entropy-dataset.txt","w")
-        intCounter = len(train_labels) 
-        for truelabel in comp_List:
-            intCounter+=1
-            f.write("%d,%s\n" % (intCounter,truelabel))
+    f= open("DT-BEST-dataset.txt","w")
+    intCounter = len(train_labels) 
+    for truelabel in comp_List:
+        intCounter+=1
+        f.write("%d,%s\n" % (intCounter,truelabel))
 
-        set_scores(eval_labels,comp_List,f)
-    else:
-        from sklearn import tree
-        clf  = tree.DecisionTreeClassifier(splitter="random")
-        df = pd.DataFrame(all_docs).to_numpy(na_value="")
-
-        # convert your list of lists into token id's. For example, 'aa bb' could be represented as a 2, a as a 1, etc.
-        tdict = corpora.Dictionary(df)
-
-        newlist = []
-        counter = 0
-        for l in df:
-            tmplist = []
-            impcount = 0
-            for word in l:
-                # append to intermediate list the id for the given word under observation
-                tmplist.append(tdict.token2id[word])
-                impcount+=1
-                if impcount>180:
-                    break
-
-            # convert to numpy array and append to main list
-            newlist.append(np.array(tmplist).astype(float)) # type float
-            print(counter)
-            counter+=1
-
-        split_point = int(0.80*len(newlist))
-        train_docs = newlist[:split_point]
-        train_labels = all_labels[:split_point]  
-        eval_docs = newlist[split_point:]
-        eval_labels = all_labels[split_point:]
-
-        
-        clf = clf.fit(train_docs,train_labels)
-        comp_List = clf.predict(eval_docs)
-
-        f= open("DT-BEST-dataset.txt","w")
-        intCounter = len(train_labels) 
-        for truelabel in comp_List:
-            intCounter+=1
-            f.write("%d,%s\n" % (intCounter,truelabel))
-
-        set_scores(eval_labels,comp_List,f)
+    set_scores(eval_labels,comp_List,f)
+    print("Printing DT-Best complete")
+    ####################### DT-Base ends
     
 
 
